f94d85c7f62c425243a4d077b51c127afbf6fecd
sapanraval <sapanraval2012@gmail.com>
2019-05-10 19:01:19 +0000

    first commit

---
+++ application.py
@@ -0,0 +1,66 @@
+from flask import Flask, abort, redirect, render_template, request, flash, url_for
+import smtplib
+from form import ContactForm
+from flask_mail import Mail, Message
+from flask_wtf import Form 
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
+import os
+
+mail = Mail()
+
+app = Flask(__name__)
+
+app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'sapanravalcs50@gmail.com'
+app.config["MAIL_PASSWORD"] = 'spring2019'
+
+mail.init_app(app)
+
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/experience")
+def experience():
+    return render_template('experience.html')
+
+@app.route("/education")
+def education():
+    return render_template('education.html')
+
+@app.route("/certification")
+def certification():
+    return render_template('certification.html')
+
+@app.route("/skills")
+def skills():
+    return render_template('skills.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            msg = Message(form.subject.data, sender='contact@example.com', recipients=['sapanravalcs50@gmail.com'])
+            msg.body = """
+            From: %s <%s>
+            %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+            
+            flash(f'Thank you for your Message {form.name.data}! I will reach out to you soon', 'success')
+            return redirect(url_for('index'))
+    return render_template('form.html',form=form)
+
+if __name__ == '__main__':
+  app.run(debug=True)
+  
+
+    
---
+++ form.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name',validators=[DataRequired(),])
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    subject = StringField('Subject',validators=[DataRequired()])    
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Submit')
